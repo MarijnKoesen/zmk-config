57682fae08071c85f05978407dd5b39356548dea
Marijn Koesen <github@koesen.nl>
2024-07-20 11:01:34 +0200

    Add backspace on homerow nav layer

--- scripts/generate_keymap_image.sh
+++ scripts/generate_keymap_image.sh
@@ -3,8 +3,8 @@
 
 cd "$(dirname "$0")/../"
 
-keymap -c .keymap.config.ferris-sweep.yaml parse -c 10 -z config/cradio.keymap > keymap.ferris-sweep.yaml 
-keymap -c .keymap.config.corne.yaml parse -c 10 -z config/corne.keymap > keymap.corne.yaml 
+keymap parse -c 10 -z config/cradio.keymap > keymap.ferris-sweep.yaml 
+keymap parse -c 12 -z config/corne.keymap > keymap.corne.yaml 
 keymap -c .keymap.config.ferris-sweep.yaml draw keymap.ferris-sweep.yaml > keymap.ferris-sweep.svg
 keymap -c .keymap.config.corne.yaml draw keymap.corne.yaml > keymap.corne.svg
 rm keymap.ferris-sweep.yaml keymap.corne.yaml
